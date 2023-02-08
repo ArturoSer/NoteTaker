a9a81b182f2c4094cb858d403703f86e6512e39f
Arturo Serrato <arturo.serrato@yahoo.com>
2023-02-07 21:18:16 -0800

    fixed routes

--- jsRoute/html.js
+++ jsRoute/html.js
@@ -1,11 +1,16 @@
+//const { appendFile } = require("fs");
 const path = require("path")
 const router = require("express").Router()
 
+
+
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "../Develop/public/index.html"))
+    res.sendFile(path.join(__dirname + "../public/index.html"))
 });
 
 router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname + "../Develop/public/notes.html"))
+    res.sendFile(path.join(__dirname + "../public/notes.html"))
 });
+
+
 module.exports = router;
